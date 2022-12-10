056691c1b48b77b9c5f7edb9aa380f4cb7561b10
Samuel Cho <cho.sam@icloud.com>
2022-12-10 10:40:16 +0100

    resolve day 1

--- day_01_calorie_counting/src/main.rs
+++ day_01_calorie_counting/src/main.rs
@@ -1,35 +1,12 @@
-use std::fs;
-
 fn main() {
-    let contents = fs::read_to_string("input.txt").unwrap();
-    let split = contents.split("\n");
-    let mut bins: Vec<Vec<&str>> = vec![Vec::new()];
-    let mut run  = 0;
-    for s in split {
-        match s {
-            s if s == "\r" || s == "" => {
-                run += 1;
-                bins.push(Vec::new());
-            },
-            _ => {
-                bins[run].push(s);
-            }
-        }
-    }
-    let mut bins = bins.into_iter().map(|bin|{
-        bin.into_iter().map(|calories|{
-            match calories.trim().parse::<i32>() {
-                Ok(v) => v,
-                Err(_) => 0
-            }
+    let contents = std::fs::read_to_string("input.txt").unwrap();
+    let mut calories = contents.split("\r\n\r\n").map(|bag|{
+        bag.lines().map(|calorie|{
+            calorie.trim().parse::<i32>().unwrap()
         }).sum()
     }).collect::<Vec<i32>>();
-    bins.sort();
-    bins.reverse();
-    //Part one
-    let most = bins[0];
-    println!("{:?}",most);
-    //Part two
-    let most:i32 = bins[..3].to_vec().iter().sum();
-    println!("{:?}",most);
+    calories.sort();
+    calories.reverse();
+    println!("Part one: {}",calories[0]);
+    println!("Part two: {}", calories[..3].iter().sum::<i32>());
 }
